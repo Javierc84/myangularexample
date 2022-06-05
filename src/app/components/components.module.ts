8333607cd85f481efd957dc3fc4c61e608016254
javier841 <jcjav84@gmail.com>
2022-06-05 09:15:11 +0200

    Formulario con validación de Login creado

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImagesComponent } from './images/images.component';
 import { NgbCarouselModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    ImagesComponent
+    ImagesComponent,
+    LoginComponent
   ],
   imports: [
     //Los módulos que necesitamos usar con la aplicación
     CommonModule,
     NgbCarouselModule,
     FormsModule,
+    ReactiveFormsModule,
     NgbModule
   ]
 })
